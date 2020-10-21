febb01213546f5a77ada7aaaeb118eda41ebb7c4
Latsuj <justal.kevin@gmail.com>
2020-10-21 21:04:20 +0800

    [FIX] Fixing variable name error panel

--- src/windowSetting.py
+++ src/windowSetting.py
@@ -3,7 +3,7 @@ import consts
 
 class WindowSetting():
     def __init__(self, right):
-        self.right = panel
+        self.right = right
 
     def show(self):
         titleblock = tkinter.Label(self.right, text="Shortcut 1", justify='left', height=2, bg=consts.BACKGROUND_COLOR, fg=consts.MENU_TEXT_COLOR, font='Helvetica 11 bold')
